8015605f65b360d95d322f98b7fb5b7d5fb0b161
Sergio <sergio@heritageafrica.com>
2014-08-06 09:48:37 +0200

    1234

--- wp-content/themes/oada/home-slider.php
+++ wp-content/themes/oada/home-slider.php
@@ -11,7 +11,15 @@ if ( $the_query->have_posts() ) { $count = 0; ?>
 		<div class="carousel-inner">
 			<?php while ( $the_query->have_posts() ) { $the_query->the_post(); $count++; ?>
 				<div class="item <?php if ($count == 1) echo 'active';?>">
-					<?php the_post_thumbnail('slide-image',$default); ?>
+					<?php $default = array('class'=>'img-responsive');?>
+					<?php $image_header = get_post_meta($post->ID,'_ppm_header_image_id',true); ?>
+					<?php if (!empty($image_header)) : ?>
+						 <?php echo wp_get_attachment_image( $image_header , 'slide-image','',$default ); ?>
+					<?php elseif (has_post_thumbnail()) : ?>
+						<?php the_post_thumbnail('slide-image',$default);?>
+					<?php else: ?>
+						<img class="img-responsive" src="http://placehold.it/1600x650&text=.">
+					<?php endif; ?>
 					<div class="carousel-caption">
 						<div class="slide-title">
 							<a href="<?php the_permalink();?>"><?php the_title();?></a>
