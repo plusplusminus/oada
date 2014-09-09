d8efd16f9b795a1bb5b3d917e9c733529a752fd6
Sergio Pellegrini <sergio@plusplusminus.co.za>
2014-09-09 18:30:09 +0200

    adss

--- wp-content/themes/oada/single-trip.php
+++ wp-content/themes/oada/single-trip.php
@@ -76,7 +76,15 @@ global $post;
 						    	<?php $map_location .= '["'.get_the_title().'", '.$location['latitude'].', '.$location['longitude'].', '.$count.',"'.get_permalink().'"],'; ?>
 						    	<div class="place col-sm-6">
 					    		<a href="<?php the_permalink();?>">
-							        <?php the_post_thumbnail('large',$default); ?>
+							        <?php $medium = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium' ); ?>
+								   	<?php $large = wp_get_attachment_image_src( get_post_thumbnail_id(), 'image-750' ); ?>
+								    <picture>
+										<!--[if IE 9]><video style="display: none;"><![endif]-->
+										<source srcset="<?php echo $medium[0]; ?>" media="(min-width: 767px)">
+										<source srcset="<?php echo $large[0]; ?>" media="(min-width: 480px)">
+										<!--[if IE 9]></video><![endif]-->
+										<img srcset="<?php echo $large[0]; ?>" alt="" class="img-responsive">
+									</picture>
 							        <div class="place-info">
 							        	<h4><?php the_title();?></h4>
 							        	<div class="inner-info">
